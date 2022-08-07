bba841298dc4175b8215e1e5fcd7a6da915a27af
Johanna Marcela Orjuela Flórez <johannaof@hotmail.com>
2022-08-07 16:36:16 -0500

    componente header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { NoticiasComponent } from './components/noticias/noticias.component';
+import { SedesComponent } from './components/sedes/sedes.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+
+const routes: Routes = [
+  {
+    path:'inicio',
+    component:InicioComponent
+  },
+  {
+    path:'servicios',
+    component:ServiciosComponent
+  },
+  {
+    path:'nosotros',
+    component:NosotrosComponent
+  },
+  {
+    path:'sedes',
+    component:SedesComponent
+  },
+  {
+    path:'noticias',
+    component:NoticiasComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatTabsModule } from '@angular/material/tabs';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { SedesComponent } from './components/sedes/sedes.component';
+import { NoticiasComponent } from './components/noticias/noticias.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    HeaderComponent,
+    InicioComponent,
+    ServiciosComponent,
+    NosotrosComponent,
+    SedesComponent,
+    NoticiasComponent
+
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
